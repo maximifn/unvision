a555f6baf85dc0fbf6fbf6c88d1bf5482cdb9788
maximstikharev <m@stikharev.ru>
2018-02-16 14:38:19 +0300

    Initial commit

---
+++ auth/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic import TemplateView
+from . import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^register/$', views.RegisterFormView.as_view()),
+    url(r'^login/$', views.LoginFormView.as_view()),
+    url(r'^logout/$', views.LogoutView.as_view()),
+]
