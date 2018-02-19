f299a8523bafdd6eacb8341458eb867117e29509
maximstikharev <m@stikharev.ru>
2018-02-19 11:21:19 +0300

    Deleted auth
    deleted auth and added to main app routing,
    Added valid static path

--- unvision/settings.py
+++ unvision/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = 'c:/unvision/static/'
-STATIC_ROOT = 'c:/unvision/static/'
+STATIC_URL = '/static/'
+# STATIC_ROOT = 'c:/unvision/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
--- unvision/urls.py
+++ unvision/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import views
+
 urlpatterns = [
-    url(r'^auth/', include('auth.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^registration/', views.RegisterFormView.as_view()),
+    url(r'^login/', views.LoginFormView.as_view()),
+    url(r'^logout/', views.LogoutView.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
 urlpatterns += staticfiles_urlpatterns()
